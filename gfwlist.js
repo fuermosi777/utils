5d13cc7a15870ae790723c3aa73d0f0aeaf626c2
Hao Liu <fuermosi777@users.noreply.github.com>
2018-09-05 15:28:24 -0700

    Create gfwlist.js

---
+++ gfwlist.js
@@ -0,0 +1,12 @@
+var rules = [
+  "*.163.com/*",
+  "*.126.net/*",
+  "*.126.com/*",
+  "*.netease.com/*",
+  "*.163music.com/*",
+  "*.126.net/*",
+  "*.163yun.com/*",
+  "*.127.net/*",
+  "*.miaozhen.com/*",
+  "*.baidu.com/*",
+];
