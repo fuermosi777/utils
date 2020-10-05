2291fa20f0ee721eb3d8c0d21030ba26bd6b350d
Hao Liu <fuermosi777@users.noreply.github.com>
2020-10-05 09:29:33 -0700

    Create things3.sh

---
+++ things3.sh
@@ -0,0 +1,22 @@
+echo "Free Things 3"
+echo "Removing current Thing 3 and its files..."
+
+osascript -e 'quit app "things"'
+
+rm -rf /Applications/Things3.app
+rm -rf ~/Library/Caches/com.culturedcode.ThingsMac
+rm -rf ~/Library/Containers/com.culturedcode.ThingsMac
+rm -rf ~/Library/Preferences/com.culturedcode.ThingsMac.plist
+
+echo "Downloading new Things 3 from the website"
+curl https://culturedcode.cachefly.net/things/Things3.zip -o ~/Desktop/Things3.zip
+
+echo "Unzipping..."
+unzip -o ~/Desktop/Things3.zip -d ~/Desktop
+
+echo "Moving to Application folder and delete zip file..."
+mv ~/Desktop/Things3.app /Applications/
+rm -rf ~/Desktop/Things3.zip
+
+echo "Done!"
+open -a Things3
