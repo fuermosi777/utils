39aa5191ae47523bcb97643aee5214255df7e7b9
Hao Liu <fuermosi777@users.noreply.github.com>
2021-06-24 09:17:50 -0700

    Update things3.sh
    Update things3 DL url

--- things3.sh
+++ things3.sh
@@ -9,7 +9,7 @@ rm -rf ~/Library/Containers/com.culturedcode.ThingsMac
 rm -rf ~/Library/Preferences/com.culturedcode.ThingsMac.plist
 
 echo "Downloading new Things 3 from the website"
-curl https://culturedcode.cachefly.net/things/Things3.zip -o ~/Desktop/Things3.zip
+curl https://static.culturedcode.com/things/Things3.zip -o ~/Desktop/Things3.zip
 
 echo "Unzipping..."
 unzip -o ~/Desktop/Things3.zip -d ~/Desktop
