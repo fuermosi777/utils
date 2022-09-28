7d2c7bd4929c14a13b5ccfa26a1fb1ded7651fac
Hao <fuermosi777@users.noreply.github.com>
2022-09-27 21:33:54 -0700

    Update init.lua

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -5,11 +5,11 @@ function chrome_switch_to(menuItem)
         local chrome = hs.appfinder.appFromName("Google Chrome")
         chrome:selectMenuItem(menuItem, true)
     end
-  end
-  
-  function chrome_active_tab_with_name(re)
-      return function()
-          hs.osascript.javascript([[
+end
+
+function chrome_active_tab_with_name(re)
+    return function()
+        hs.osascript.javascript([[
               var chrome = Application('Google Chrome');
               chrome.activate();
               var wins = chrome.windows;
@@ -30,30 +30,30 @@ function chrome_switch_to(menuItem)
               }
               main();
           ]])
-      end
-  end
-  
-  function open(name)
+    end
+end
+
+function open(name)
     return function()
         hs.application.launchOrFocus(name)
         if name == 'Finder' then
             hs.appfinder.appFromName(name):activate()
         end
     end
-  end
+end
 
-  function openChromeApp(name)
+function openChromeApp(name)
     return function()
         -- Be sure to get the real name of the app (Use ls -a to check).
         hs.application.launchOrFocus(os.getenv('HOME') .. '/Applications/Chrome Apps.localized/' .. name .. '.app')
     end
-  end
-  
-  function sleep()
+end
+
+function sleep()
     hs.caffeinate.systemSleep()
-  end
-  
-  function move(dir)
+end
+
+function snap(dir)
     return function()
         local win = hs.window.focusedWindow()
         local frame = win:frame()
@@ -64,152 +64,164 @@ function chrome_switch_to(menuItem)
         local y = frame.y
         local w = frame.w
         local h = frame.h
-  
+
         if dir == 'right' then
-            if x < 0 then -- negative left
+            if x < 0 then -- overflow left
                 frame.x = 0
-            elseif x == 0 then -- attach to left
-                if w < screenFrame.w * 1 / 4 then -- win width less than 25%
+                frame.w = math.min(frame.w, screenFrame.w)
+            elseif x == 0 then -- attached left, expand to full and then shrink
+                if w < screenFrame.w * 1 / 4 then -- win width less than 33%
                     frame.w = screenFrame.w * 1 / 4
-                elseif w < math.floor(screenFrame.w * 1 / 3) then -- win with less than 33%
-                    frame.w = math.floor(screenFrame.w * 1 / 3)
                 elseif w < screenFrame.w * 1 / 2 then -- win width less than 50%
                     frame.w = screenFrame.w * 1 / 2
-                elseif w < screenFrame.w * 3 / 4 then -- win with less than 75%
+                elseif w < screenFrame.w * 3 / 4 then -- win with less than 67%
                     frame.w = screenFrame.w * 3 / 4
-                else
-                    frame.x = screenFrame.w * 1 / 4
+                elseif w < screenFrame.w then -- win not full screen
+                    frame.w = screenFrame.w
+                else -- already full with, start shrink
                     frame.w = screenFrame.w * 3 / 4
+                    frame.x = screenFrame.w - frame.w
                 end
-            elseif x < screenFrame.w / 2 then -- win on the left side
-                frame.x = screenFrame.w / 2
-                frame.w = screenFrame.w / 2
-            elseif x >= screenFrame.w / 2 and x < math.floor(screenFrame.w * 2 / 3) then -- win left edge in 50% - 66%
-                frame.x = math.floor(screenFrame.w * 2 / 3)
-                frame.w = math.floor(screenFrame.w * 1 / 3)
-            elseif x >= math.floor(screenFrame.w * 2 / 3) and x < screenFrame.w * 3 / 4 then -- win left edge in 66% - 75%
-                frame.x = screenFrame.w * 3 / 4
-                frame.w = screenFrame.w * 1 / 4
+            elseif x + frame.w < screenFrame.w then -- gap on right side, attaching right
+                frame.x = screenFrame.w - frame.w
+            else -- already attached right or overflow right, shrink
+                frame.w = screenFrame.w - x
+                if w > screenFrame.w * 3 / 4 then
+                    frame.w = screenFrame.w * 3 / 4
+                    frame.x = screenFrame.w - frame.w
+                elseif w > screenFrame.w * 1 / 2 then
+                    frame.w = screenFrame.w * 1 / 2
+                    frame.x = screenFrame.w - frame.w
+                elseif w > screenFrame.w * 1 / 3 then
+                    frame.w = screenFrame.w * 1 / 3
+                    frame.x = screenFrame.w - frame.w
+                end
             end
         elseif dir == 'left' then
-            if x + w > screenFrame.w  then -- negative right
+            if x + w > screenFrame.w then -- overflow right, un-overflow
                 frame.x = screenFrame.w - w
-            elseif x + w == screenFrame.w then -- attach to right
-                if w < screenFrame.w * 1 / 4 then -- win width less than 25%
+                frame.w = math.min(frame.w, screenFrame.w)
+            elseif x + w == screenFrame.w then -- attached right, expand to full then shrink
+                if w < screenFrame.w * 1 / 4 then -- win with less than 33%
                     frame.w = screenFrame.w * 1 / 4
-                    frame.x = screenFrame.w - frame.w
-                elseif w < math.floor(screenFrame.w * 1 / 3) then -- win with less than 33%
-                    frame.w = math.floor(screenFrame.w * 1 / 3)
                     frame.x = screenFrame.w - frame.w
                 elseif w < screenFrame.w * 1 / 2 then -- win width less than 50%
                     frame.w = screenFrame.w * 1 / 2
                     frame.x = screenFrame.w - frame.w
-                elseif w < screenFrame.w * 3 / 4 then -- win with less than 75%
+                elseif w < screenFrame.w * 3 / 4 then -- win with less than 67%
                     frame.w = screenFrame.w * 3 / 4
                     frame.x = screenFrame.w - frame.w
-                else
-                    frame.x = 0
+                elseif w < screenFrame.w then -- win almost full width
+                    frame.w = screenFrame.w
+                    frame.x = screenFrame.w - frame.w
+                else -- full width, start shrinking
                     frame.w = screenFrame.w * 3 / 4
                 end
-            elseif x > screenFrame.w / 2 then -- win on the right side
+            elseif x > 0 then -- gap on left
                 frame.x = 0
-                frame.w = screenFrame.w / 2
-            elseif x > 0 then -- win on left side
+            else -- already attached left or overflow left
                 frame.x = 0
-            elseif w > screenFrame.w / 2 then -- win larger than 50%
-                frame.w = screenFrame.w * 1 / 2
-            elseif w > math.floor(screenFrame.w * 1 / 3) then -- win larger than 33%
-                frame.w = math.floor(screenFrame.w * 1 / 3)
-            elseif w > screenFrame.w * 1 / 4 then -- win larger than 25%
-                frame.w = screenFrame.w * 1 / 4
+                if w > screenFrame.w * 3 / 4 then
+                    frame.w = screenFrame.w * 3 / 4
+                elseif w > screenFrame.w * 1 / 2 then
+                    frame.w = screenFrame.w * 1 / 2
+                elseif w > screenFrame.w * 1 / 3 then
+                    frame.w = screenFrame.w * 1 / 3
+                end
             end
         elseif dir == 'up' then
-            if y == screenFrame.y then -- attach to top
+            if y == screenFrame.y then -- attached to top
                 if h >= screenFrame.h / 2 then
                     frame.h = screenFrame.h / 2
                 end
-            elseif y + h == screenFrame.h + screenFrame.y then -- attach to bottom
-                frame.y = 0
-                frame.h = screenFrame.h
-            else
-                frame.y = 0
+            elseif y + h == screenFrame.h + screenFrame.y then -- attached to bottom
+                if h < math.floor(screenFrame.h / 2) then
+                    frame.h = math.floor(screenFrame.h / 2)
+                else
+                    frame.y = screenFrame.y
+                    frame.h = screenFrame.h
+                end
+            else -- not attached
+                frame.y = screenFrame.y
             end
         elseif dir == 'down' then
             if y + h == screenFrame.h + screenFrame.y then -- attach to bottom
-                if h >= screenFrame.h / 2 then
-                    frame.h = screenFrame.h / 2
+                if h > math.floor(screenFrame.h / 2) then
+                    frame.h = math.floor(screenFrame.h / 2)
                     frame.y = screenFrame.h - frame.h + screenFrame.y
                 end
             elseif y == screenFrame.y then -- attach to top
-                frame.y = 0
+                frame.y = screenFrame.y
                 frame.h = screenFrame.h
-            elseif y + h > screenFrame.h then -- overflow bottom do nothing
+            elseif y + h > screenFrame.h + screenFrame.y then -- overflow bottom do nothing
             else
-                frame.y = screenFrame.h - h + 22
+                frame.y = screenFrame.h - h + screenFrame.y
             end
         end
-  
+
         frame = win:screen():localToAbsolute(frame)
         win:setFrame(frame)
     end
-  end
-  
-  function send_window_prev_monitor()
-  local win = hs.window.focusedWindow()
-  local nextScreen = win:screen():previous()
-  win:moveToScreen(nextScreen)
-  end
-  
-  function send_window_next_monitor()
-  local win = hs.window.focusedWindow()
-  local nextScreen = win:screen():next()
-  win:moveToScreen(nextScreen)
-  end
-  
-  --- open different Chrome users
-  hs.hotkey.bind({"alt"}, "1", chrome_switch_to({"Profiles", "Hao"}))
-  hs.hotkey.bind({"alt"}, "2", chrome_switch_to({"Profiles", "Hao (Personal)"}))
-  hs.hotkey.bind({"alt"}, "`", chrome_switch_to({"File", "New Incognito Window"}))
-  
-  --- quick open applications
-  hs.hotkey.bind({"alt"}, "E", open("Finder"))
-  hs.hotkey.bind({"alt"}, "W", open("WeChat"))
-  hs.hotkey.bind({"alt"}, "C", open("Google Chrome"))
-  hs.hotkey.bind({"alt"}, "T", open("iTerm"))
-  hs.hotkey.bind({"alt"}, "X", open("Xcode"))
-  hs.hotkey.bind({"alt"}, "S", open("Sublime Text"))
-  hs.hotkey.bind({"alt"}, "V", open("Visual Studio Code"))
-  hs.hotkey.bind({"alt"}, "H", open("Things3"))
-  hs.hotkey.bind({"alt"}, "D", openChromeApp("Cider-V"))
-  hs.hotkey.bind({"alt"}, "A", openChromeApp("Google Chat"))
-  --- sleep
-  hs.hotkey.bind({"shift", "alt", "command"}, "DELETE", sleep)
-  
-  --- window
-  hs.window.animationDuration = 0
-  hs.hotkey.bind({"alt", "cmd"}, "Right", move('right'))
-  hs.hotkey.bind({"alt", "cmd"}, "Left", move('left'))
-  hs.hotkey.bind({"alt", "cmd"}, "Up", move('up'))
-  hs.hotkey.bind({"alt", "cmd"}, "Down", move('down'))
-  hs.hotkey.bind({"shift", "alt", "cmd"}, "Left", send_window_prev_monitor)
-  hs.hotkey.bind({"shift", "alt", "cmd"}, "Right", send_window_next_monitor)
-  
-  --- when connected to work Wifi, mute the computer
-  local workWifi = 'Google-A'
-  local outputDeviceName = 'Built-in Output'
-  hs.wifi.watcher.new(function()
+end
+
+function send_window_prev_monitor()
+    local win = hs.window.focusedWindow()
+    local nextScreen = win:screen():previous()
+    win:moveToScreen(nextScreen)
+end
+
+function send_window_next_monitor()
+    local win = hs.window.focusedWindow()
+    local nextScreen = win:screen():next()
+    win:moveToScreen(nextScreen)
+end
+
+--- open different Chrome users
+hs.hotkey.bind({"alt"}, "1", chrome_switch_to({"Profiles", "Hao"}))
+hs.hotkey.bind({"alt"}, "2", chrome_switch_to({"Profiles", "Hao (Personal)"}))
+hs.hotkey.bind({"alt"}, "`", chrome_switch_to({"File", "New Incognito Window"}))
+
+--- quick open applications
+hs.hotkey.bind({"alt"}, "E", open("Finder"))
+hs.hotkey.bind({"alt"}, "W", open("WeChat"))
+hs.hotkey.bind({"alt"}, "C", open("Google Chrome"))
+hs.hotkey.bind({"alt"}, "T", open("iTerm"))
+hs.hotkey.bind({"alt"}, "X", open("Xcode"))
+hs.hotkey.bind({"alt"}, "S", open("Sublime Text"))
+hs.hotkey.bind({"alt"}, "V", open("Visual Studio Code"))
+hs.hotkey.bind({"alt"}, "H", open("Things3"))
+hs.hotkey.bind({"alt"}, "D", openChromeApp("Cider-V"))
+hs.hotkey.bind({"alt"}, "A", openChromeApp("Google Chat"))
+--- sleep
+hs.hotkey.bind({"shift", "alt", "command"}, "DELETE", sleep)
+
+--- window
+hs.window.animationDuration = 0
+hs.hotkey.bind({"alt", "cmd"}, "Right", snap('right'))
+hs.hotkey.bind({"alt", "cmd"}, "Left", snap('left'))
+hs.hotkey.bind({"alt", "cmd"}, "Up", snap('up'))
+hs.hotkey.bind({"alt", "cmd"}, "Down", snap('down'))
+hs.hotkey.bind({"shift", "alt", "cmd"}, "Left", send_window_prev_monitor)
+hs.hotkey.bind({"shift", "alt", "cmd"}, "Right", send_window_next_monitor)
+
+--- when connected to work Wifi, mute the computer
+local workWifi = 'Google-A'
+local outputDeviceName = 'Built-in Output'
+hs.wifi.watcher.new(function()
     local currentWifi = hs.wifi.currentNetwork()
     local currentOutput = hs.audiodevice.current(false)
-    if not currentWifi then return end
+    if not currentWifi then
+        return
+    end
     if (currentWifi == workWifi and currentOutput.name == outputDeviceName) then
         hs.audiodevice.findDeviceByName(outputDeviceName):setOutputMuted(true)
     end
-  end):start()
-  
-  --- key macros
-  function keyStrokes(str)
+end):start()
+
+--- key macros
+function keyStrokes(str)
     return function()
         hs.eventtap.keyStrokes(str)
     end
-  end
-  hs.hotkey.bind({"alt", "cmd"}, "L", keyStrokes("console.log("))
+end
+hs.hotkey.bind({"alt", "cmd"}, "L", keyStrokes("console.log("))
